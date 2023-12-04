8866d288a23c30cf5e6c8b6bcdd2cd2a7e88f021
Christian Tsoungui NKoulou <tsounguc@mail.gvsu.edu>
2023-12-04 10:14:53 -0500

    Create Forms with Flask-WTF

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
+import os
+
 from flask import Flask, render_template
+from flask_wtf import FlaskForm
 from wtforms import StringField
-from wtforms.validators import DataRequired
+
+from login_form import LoginForm
 
 '''
 Red underlines? Install the required packages first: 
@@ -15,8 +19,8 @@ pip3 install -r requirements.txt
 This will install the packages from requirements.txt for this project.
 '''
 
-
 app = Flask(__name__)
+app.secret_key = os.environ.get("secret_key", "Couldn't find secret_key")
 
 
 @app.route("/")
@@ -24,9 +28,12 @@ def home():
     return render_template('index.html')
 
 
-@app.route("/login")
+@app.route("/login", methods=['GET', 'POST'])
 def login():
-    return render_template('login.html')
+    form = LoginForm()
+
+    return render_template('login.html', form=form)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
